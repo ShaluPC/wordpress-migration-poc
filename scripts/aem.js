cbc9bcd6ffcddff5d4f86a6b04178f6c4a600d95
HRBINC\A2612560 <shalu.chacko@hrblock.com>
2026-08-03 14:25:17 +0530

    .

--- scripts/aem.js
+++ scripts/aem.js
@@ -507,7 +507,7 @@ function decorateSections(main) {
           section.dataset[toCamelCase(key)] = meta[key];
         }
       });
-      if (section.dataset.articleDetailBar === 'true') {
+      if (section.dataset.articledetailbar === 'true') {
         section.classList.add('article-body', 'tic-article-detail-bar', 'tic-article-detail-bar--desktop');
       }
       sectionMeta.parentNode.remove();
